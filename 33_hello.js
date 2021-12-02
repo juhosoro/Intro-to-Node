67fb6ecd71250338118a0413465214d583967a12
Juho Soronen <juhosoroo@gmail.com>
2021-12-02 11:42:40 +0200

    removed unnecessary versions of hello.js

--- 33_hello.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<h1>Hello World! My Name Is John Elder!</h1>');
-}).listen(8080);
